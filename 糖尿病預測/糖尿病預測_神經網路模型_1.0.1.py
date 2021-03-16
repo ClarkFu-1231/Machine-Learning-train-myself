cc8f72b77338683f8b1d6bb8007620f96eda7cef
ClarkFu-1231 <10746014@ntub.edu.tw>
2021-03-16 15:02:37 +0800

    基礎神經網路練習

---
+++ 糖尿病預測/糖尿病預測_神經網路模型_1.0.1.py
@@ -0,0 +1,37 @@
+'''特徵標準化'''
+import keras
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+np.random.seed(10)
+
+df= pd.read_csv("./diabetes.csv")
+dataset= df.values
+np.random.shuffle(dataset)
+
+X= dataset[:, 0:8]
+Y= dataset[:, 8]
+
+'''定義模型'''
+model= Sequential()
+model.add(Dense(10, input_shape=(8, ), activation= "relu"))
+model.add(Dense(8, activation= "relu"))
+model.add(Dense(1, activation= "sigmoid"))
+model.summary()
+
+'''編譯模型'''
+model.compile(loss="binary_crossentropy", optimizer="sgd",
+              metrics=["accuracy"])
+
+'''特徵標準化'''
+X -= X.mean(axis=0)
+X /= X.std(axis=0)
+ 
+'''訓練模型'''
+model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
+
+'''評估模型'''
+loss, accuracy= model.evaluate(X, Y)
+print("準確度={:.2f}".format(accuracy))
+
