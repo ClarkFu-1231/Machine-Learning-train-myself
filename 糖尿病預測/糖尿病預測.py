033f91a6e8f7926233405476b259a900ed5c845a
ClarkFu-1231 <10746014@ntub.edu.tw>
2021-03-17 21:48:15 +0800

    更改資料夾名稱

--- 糖尿病預測/糖尿病預測.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("./diabetes.csv")
-"""
-9個資料欄位(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age, Outcome)
-"""
-print(df.head())
-print(df.shape)#shape屬性顯示資料集有幾筆紀錄和幾個欄位，也就是這個資料表的形狀
